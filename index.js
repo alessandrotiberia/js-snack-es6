28028e6c6364f9bd72acd947ae2aea66017ac90d
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-09 15:29:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+ nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+'use strict';
+
+
+const bici = [
+ {
+    nome: "bici0", 
+    peso: 10
+},
+{
+    nome: "bici1",
+    peso: 20
+},
+{
+    nome: "bici2",
+    peso: 5 
+}
+];
