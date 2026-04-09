4756bf93e8846b75e70f6e5e8b61487fd2c19983
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-09 15:48:17 +0200

    esercizio 1 completato

--- index.js
+++ index.js
@@ -7,20 +7,32 @@ Stampare a schermo la bici con peso minore.*/
 
 //creato array oggetti bici
 const bici = [
- {
-    nome: "bici0", 
-    peso: 10,
-},
-{
-    nome: "bici1",
-    peso: 20,
-},
-{
-    nome: "bici2",
-    peso: 5 ,
-},
-{
-    nome: "bici3",
-    peso: 15
-}
+    {
+        nome: "bici0",
+        peso: 10,
+    },
+    {
+        nome: "bici1",
+        peso: 20,
+    },
+    {
+        nome: "bici2",
+        peso: 5,
+    },
+    {
+        nome: "bici3",
+        peso: 15
+    }
 ];
+
+//creato variabile per bici più leggera
+let bici_leggera = bici[0];
+
+//ciclo for per confrontare peso bici
+for (let i = 1; i < bici.length; i++) {
+    //confronto peso bici
+if (bici[i].peso < bici_leggera.peso) {
+    //assegno bici più leggera
+    bici_leggera = bici[i];
+} //stampo bici leggera con peso
+} console.log(bici_leggera.peso);
