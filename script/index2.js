66df22e6af86433822068d4fbd14d953f3624f4a
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-09 16:22:17 +0200

    esercizio 2 con array e ciclo for punti e falli random

--- script/index2.js
+++ script/index2.js
@@ -4,3 +4,37 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+
+//creato array 
+squadre = [
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Napoli',
+        punti: 0,
+        falliSubiti: 0
+    },
+]
+
+//generato numeri random
+for (let i = 0; i < squadre.length; i++) { 
+    //punti random arrotondati a squadra 
+    squadre[i].punti = Math.floor(Math.random() * 100);
+    //falli subiti random arrotondati a squadra
+    squadre[i].falliSubiti = Math.floor(Math.random() * 100);
+}
+// come creo nuovo array per inserire solo nomi e falli subiti?
